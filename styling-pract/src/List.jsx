013d429c6a14c48687022df33400c1a69198851f
IvanR96 <irios96@hotmail.com>
2025-11-24 23:13:22 -0800

    list component

---
+++ styling-pract/src/List.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function List(){
+  return <div>
+  <h2>My Favorite Foods</h2>
+  <ul>
+    <li>Pizza</li>
+    <li>Spaghetti</li>
+    <li>Tacos</li>
+  </ul>
+  </div>
+};
+
+export default List;
