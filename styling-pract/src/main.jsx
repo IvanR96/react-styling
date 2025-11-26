b2c71e7b0eea1ae19e01bc896dae56db0d324ed1
IvanR96 <irios96@hotmail.com>
2025-11-25 23:16:24 -0800

    App called as component

--- styling-pract/src/main.jsx
+++ styling-pract/src/main.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import List from './List';
+import App from './App';
 
-const currentHour = new Date().getHours();
-
-console.log(currentHour);
-let greeting;
-let color;
-
-
-  if (currentHour < 12) {
-    greeting = "Good Morning!";
-    color= "red";
-  } else if (currentHour >= 12 && currentHour < 17) {
-    greeting = "Good Afternoon!";
-    color = "green";
-  } else {
-    greeting = "Good Night!";
-    color = "blue";
-  }
 
 
 createRoot(document.getElementById('root')).render(
   <div>
-    <h1 className='heading' style={{color}}>{greeting}</h1>
-    <List></List>
+    <App></App>
   </div>
 )
