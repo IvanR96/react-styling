d32c6bbdb510e0d93f69cae95f96e5a7c57217c4
IvanR96 <irios96@hotmail.com>
2025-11-25 23:15:53 -0800

    main app jsx file

---
+++ styling-pract/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import './index.css';
+import { createRoot } from "react-dom/client";
+import List from './List';
+
+function App(){
+const currentHour = new Date().getHours();
+let greeting;
+let color;
+
+
+
+  if (currentHour < 12) {
+    greeting = "Good Morning!";
+    color= "red";
+  } else if (currentHour >= 12 && currentHour < 17) {
+    greeting = "Good Afternoon!";
+    color = "green";
+  } else {
+    greeting = "Good Night!";
+    color = "blue";
+  }
+
+
+createRoot(document.getElementById('root')).render(
+  <div>
+    <h1 className='heading' style={{color}}>{greeting}</h1>
+    <List></List>
+  </div>
+)
+
+}
+
+export default App;
