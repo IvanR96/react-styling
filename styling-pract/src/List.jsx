404c5d3d596b298cac848cc968f107fc8c1df3ed
IvanR96 <irios96@hotmail.com>
2025-11-27 00:02:39 -0800

    calculator imported to List

--- styling-pract/src/List.jsx
+++ styling-pract/src/List.jsx
@@ -1,12 +1,14 @@
 import React from "react"
+import add, {multiply, subtract, divide} from "./Calculator";
 
 function List(){
   return <div>
   <h2>My Favorite Foods</h2>
   <ul>
-    <li>Pizza</li>
-    <li>Spaghetti</li>
-    <li>Breaded Shrimp</li>
+    <li>{add(1,2)}</li>
+    <li>{multiply(2,3)}</li>
+    <li>{subtract(7,2)}</li>
+    <li>{divide(5,2)}</li>
   </ul>
   </div>
 };
